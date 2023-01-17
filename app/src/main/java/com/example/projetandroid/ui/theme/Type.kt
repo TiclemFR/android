ed76b9713e539e2ae9bdc1763f24b2690375579b
theog <theoxgamerz@gmail.com>
2023-01-17 10:58:59 +0100

    Page "Se connecter".

--- app/src/main/java/com/example/projetandroid/ui/theme/Type.kt
+++ app/src/main/java/com/example/projetandroid/ui/theme/Type.kt
@@ -2,27 +2,20 @@ package com.example.projetandroid.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.projetandroid.R
 
 // Set of Material typography styles to start with
+val Inter = FontFamily(Font(R.font.inter_medium));
+val InterCF = FontFamily(Font(R.font.fontspring_demo_integralcf_medium))
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = Inter
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    body2 = TextStyle(
+        fontFamily = InterCF
     )
-    */
 )
