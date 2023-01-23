27e96288d08ebb34c2ea5815135d8738c2cbbe7a
TiclemFR <grandfiston@gmail.com>
2023-01-23 16:20:58 +0100

    Repair build

--- app/src/main/java/com/example/projetandroid/MainActivity.kt
+++ app/src/main/java/com/example/projetandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.projetandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -12,15 +13,17 @@ import com.example.projetandroid.BonPlan.BonPlan
 import com.example.projetandroid.login.loginPage
 import com.example.projetandroid.onBoarding.OnBoarding
 import com.example.projetandroid.ui.theme.*
+import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import connectPage
 
 class MainActivity : ComponentActivity() {
     lateinit var navController:NavController
-    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        val db = Firebase.firestore
         setContent {
             ProjetAndroidTheme {
                 navController = rememberNavController()
