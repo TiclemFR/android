5a03e4f5f6ba2977aae860a56305a6e0c058ddc6
theog <theoxgamerz@gmail.com>
2023-01-24 08:47:33 +0100

    Home page

--- app/src/main/java/com/example/projetandroid/MainActivity.kt
+++ app/src/main/java/com/example/projetandroid/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.projetandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.projetandroid.ui.theme.*
 import connectPage
+import home
+import navbar
+import previewCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProjetAndroidTheme {
-                connectPage()
+                home()
             }
         }
     }
