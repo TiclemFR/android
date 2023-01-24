fbf307431ebd222cc77a582957da29367633fe73
TiclemFR <grandfiston@gmail.com>
2023-01-24 09:51:25 +0100

    Finish onBoarding

--- app/src/main/java/com/example/projetandroid/onBoarding/onBoarding.kt
+++ app/src/main/java/com/example/projetandroid/onBoarding/onBoarding.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
@@ -24,6 +25,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 import com.example.projetandroid.ui.theme.*
+import previewCard
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -38,7 +40,7 @@ fun OnBoarding(navController: NavController){
         Row(horizontalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 98.dp)) {
+            .padding(top = 50.dp)) {
             Text(text = "Pas de sous ?", style = Typography.h1, color = Color.White, textAlign = TextAlign.Center)
         }
         Row(horizontalArrangement = Arrangement.Center,
@@ -51,7 +53,7 @@ fun OnBoarding(navController: NavController){
 
         Row(horizontalArrangement = Arrangement.Center,
             modifier = Modifier
-                .padding(top = 78.dp)
+                .padding(top = 52.dp)
         ) {
             HorizontalPagerIndicator(
                 pagerState = pagerState,
@@ -64,20 +66,81 @@ fun OnBoarding(navController: NavController){
         }
         Row(horizontalArrangement = Arrangement.Center) {
             Column(modifier = Modifier
-                .padding(top = 28.dp)
+                .fillMaxWidth()
+                .height(380.dp)
+                .padding(top = 28.dp, start = 30.dp, end = 30.dp)
                 .clip(shape = RoundedCornerShape(20.dp))
                 .background(BackgroundColor)
+
             ){
                 Row(horizontalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .padding(top = 20.dp)
-                        .width(265.dp)
-                        .height(245.dp)
                 ){
                     HorizontalPager(count = 3,
                         state = pagerState) { page ->
 
                         when (page) {
+                            0 -> {
+                                Row() {
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                }
+                            }
+                            1 -> {
+                                Row() {
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                }
+                            }
+                            2 -> {
+                                Row() {
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                    Column() {
+                                        Row {
+                                            previewCard()
+                                        }
+                                        Row {
+                                            previewCard()
+                                        }
+                                    }
+                                }
+                            }
                         }
 
                     }
