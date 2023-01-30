b8b360563b04616680f141a4dc927b80f727c624
TiclemFR <grandfiston@gmail.com>
2023-01-30 09:24:05 +0100

    Create card in firebase

--- app/src/main/java/com/example/projetandroid/models/Card.kt
+++ app/src/main/java/com/example/projetandroid/models/Card.kt
@@ -1,5 +1,12 @@
 package com.example.projetandroid.models
 
+import com.google.firebase.auth.FirebaseUser
+
 data class Card(
-    val title:String = ""
+    var title:String? = "",
+    var resume:String? = "",
+    var link:String? = "",
+    var image:String? = "",
+    var star:Number? = 0,
+    var user:FirebaseUser? = null
 )
